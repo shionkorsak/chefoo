e1973afa3b6cebf251f74b0de1e5353dd1fd6f54
Ryosei Inagaki <62004600+gakiw0@users.noreply.github.com>
2025-04-22 15:59:53 +0800

    edited border_text_field
    you can change its colors now

--- lib/widgets/border_text_field.dart
+++ lib/widgets/border_text_field.dart
@@ -4,29 +4,40 @@ import 'package:flutter_skeleton/commons.dart';
 class BorderTextField extends StatelessWidget {
   final String label;
   final ValueChanged<String>? onChanged;
+  final Color? borderColor;
+  final Color? focusedBorderColor;
+  final Color? labelColor;
+  final Color? cursorColor;
 
   const BorderTextField({
     Key? key,
     required this.label,
     this.onChanged,
+    this.borderColor,
+    this.focusedBorderColor,
+    this.labelColor,
+    this.cursorColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      cursorColor: AppColors.textPrimary,
+      cursorColor: cursorColor ?? AppColors.textPrimary,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderSide: BorderSide(),
         ),
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: AppColors.textSecondary),
-            borderRadius: kRadius10),
+          borderSide: BorderSide(color: borderColor ?? AppColors.textSecondary),
+          borderRadius: kRadius10,
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: AppColors.textSecondary),
-            borderRadius: kRadius10),
+          borderSide:
+              BorderSide(color: focusedBorderColor ?? AppColors.textSecondary),
+          borderRadius: kRadius10,
+        ),
         labelText: label,
-        labelStyle: TextStyle(color: AppColors.textSecondary),
+        labelStyle: TextStyle(color: labelColor ?? AppColors.textSecondary),
       ),
       onChanged: onChanged,
     );
