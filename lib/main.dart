691d4df4efac96ef55f2e5831731e6c496629699
laurashion <laura@gapp.nthu.edu.tw>
2025-04-28 17:59:11 +0800

    update main.dart w message

--- lib/main.dart
+++ lib/main.dart
@@ -57,5 +57,7 @@ class MyApp extends StatelessWidget {
         // routes: {Screens.profile: (_) => const ProfileScreen()},
         //home: GetStarted());
         home: TestScreen());
+          // this testScreen is only to visualize google maps info
+          // which we are importing, and related widgets
   }
 }
