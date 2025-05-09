abdd2c72fa9ed0f593f0488ec821ed7eee6e07e5
Ryosei Inagaki <62004600+gakiw0@users.noreply.github.com>
2025-05-09 17:49:07 +0800

    I don't know what's going on. I am tired.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:chefoo/providers/favorites.dart';
 import 'package:chefoo/screens/login/login_screen.dart';
 import 'package:chefoo/screens/testScreen.dart';
+import 'package:chefoo/screens/tests/widget_test_screen.dart';
 import 'package:chefoo/screens/welcome/get_started.dart';
 import 'package:chefoo/screens/welcome_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -70,8 +71,8 @@ class MyApp extends StatelessWidget {
 
         // routes: {Screens.profile: (_) => const ProfileScreen()},
         //home: GetStarted());
-        //home: WidgetTestScreen());
-      home: TestScreen());       
+        home: WidgetTestScreen());
+      // home: TestScreen());       
         // this testScreen is only to visualize google maps info
         // which we are importing, and related widgets
   }
