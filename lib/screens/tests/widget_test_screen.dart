a50880c9029f4417e4b8eb60ec61a24f9c039b60
Ryosei Inagaki <62004600+gakiw0@users.noreply.github.com>
2025-05-19 23:03:42 +0800

    Update custom_navigation _bar.dart
    Please check widget_test_screen.dart to understand how to use this widget because this cannot be implemented in the same way as other navbars.

--- lib/screens/tests/widget_test_screen.dart
+++ lib/screens/tests/widget_test_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chefoo/widgets/ai_input_field.dart';
 import 'package:chefoo/widgets/buttons/arrow_button.dart';
+import 'package:chefoo/widgets/custom_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:chefoo/commons.dart';
 import 'package:chefoo/widgets/border_text_field.dart';
@@ -12,65 +13,88 @@ import 'package:chefoo/widgets/cards/restaurant_card_vertical.dart';
 import 'package:chefoo/widgets/dots_page_indicator.dart';
 import 'package:chefoo/widgets/tags/tag_map.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
+import 'package:chefoo/widgets/custom_bottom_navigation_bar.dart';
 
-class WidgetTestScreen extends StatelessWidget {
+class WidgetTestScreen extends StatefulWidget {
   WidgetTestScreen({super.key});
+
+  @override
+  State<WidgetTestScreen> createState() => _WidgetTestScreenState();
+}
+
+class _WidgetTestScreenState extends State<WidgetTestScreen> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AuthCard(
+      body: SafeArea(
+        child: Stack(children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  BorderTextField(label: 'Enter your text here'),
-                  SizedBox(
-                    height: 32,
+                  AuthCard(
+                    children: [
+                      BorderTextField(label: 'Enter your text here'),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      GlowingButton(onPressed: () {}, text: 'Get Started'),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      TagMap(tags: ['aaaa', 'bbbb', 'cccc']),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      GoogleLoginButton(),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      GoogleImportButton(),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.5,
+                      ),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      DotsPageIndicator(pageCount: 5, currentPage: 4),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      ArrowButton(
+                        onPressed: () {},
+                      ),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      AIInputField(),
+                    ],
                   ),
-                  GlowingButton(onPressed: () {}, text: 'Get Started'),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  TagMap(tags: ['aaaa', 'bbbb', 'cccc']),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  GoogleLoginButton(),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  GoogleImportButton(),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  LinearProgressIndicator(
-                    value: 0.5,
-                  ),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  DotsPageIndicator(pageCount: 5, currentPage: 4),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  ArrowButton(
-                    onPressed: () {},
-                  ),
-                  SizedBox(
-                    height: 32,
-                  ),
-                  AIInputField(),
                 ],
               ),
-
-              // RestaurantCardVertical()
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: CustomBottomNavigationBar(
+              currentIndex: _selectedIndex,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ]),
       ),
     );
   }
